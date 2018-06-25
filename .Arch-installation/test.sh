24e61e8c131abf88059e3e3badfe0a7b8341ccb7
moroclash <mmoroclash@gmail.com>
2018-06-25 06:34:51 +0000

    some test cases

--- .Arch-installation/test.sh
+++ .Arch-installation/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #test .sh script
-Testfile="../test/test.text"
+#Testfile="../test/test.text"
 
 #touch $Testfile
 #echo "hellow" >> $Testfile
@@ -13,8 +13,9 @@ Testfile="../test/test.text"
 #echo $Testfile " aaaa"
 #echo "adding hostname as '"$Testfile"'"
 
-echo "Adding new user '"${Testfile}"'"
+#echo "Adding new user '"${Testfile}"'"
 
-echo "[archlinuxfr]
-SigLevel=Never
-Server=http://repo.archlinux.fr/$arch" >>  $Testfile
+#echo "[archlinuxfr]
+#SigLevel=Never
+#Server=http://repo.archlinux.fr/$arch" >>  $Testfile
+
